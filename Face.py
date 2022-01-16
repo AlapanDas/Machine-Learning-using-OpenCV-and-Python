904714c14bbb8ee0e5c60f4ca85818dc14b97e22
Alapan <alapandas333@gmail.com>
2022-01-16 12:03:19 +0530

    Added Face Recognition Feature

--- Face.py
+++ Face.py
@@ -1,12 +1,8 @@
 import cv2
 from matplotlib.image import imread
-
 face=cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
-
-cap=cv2.VideoCapture(r'C:\Users\clash\Desktop\Git\ML in Python\Pro.mp4')
-
+cap=cv2.VideoCapture(r'C:\Users\clash\Documents\ML in Python\Pro.mp4')
 #img=cv2.imread('ML in Python\img.jpg')
-
 while cap.isOpened():
     _, img=cap.read()
     gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
@@ -16,4 +12,5 @@ while cap.isOpened():
     cv2.imshow('img',img)
     if cv2.waitKey(1) & 0xFF ==ord('q') :
         break   
+
 cap.release()
