fd1ddf59806fec9ad862a94deb8096a9894764a4
Alapan <alapandas333@gmail.com>
2022-04-26 00:22:49 +0530

    Modified

--- Face.py
+++ Face.py
@@ -2,7 +2,7 @@ import cv2
 from matplotlib.image import imread
 face=cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
 eye=cv2.CascadeClassifier('haarcascade_eye_tree_eyeglasses.xml')
-cap=cv2.VideoCapture(r'C:\Users\clash\Documents\ML in Python\Pro.mp4')
+cap=cv2.VideoCapture(r'D:\Documents\ML in Python\Pro.mp4')
 #img=cv2.imread('ML in Python\img.jpg')
 while cap.isOpened():
     _, img=cap.read()
